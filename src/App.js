a2c206af13c3ae8ac095566dcc63629929ca640b
Zamirabei <bellofachoz@gmail.com>
2024-05-11 01:03:45 +0600

    month_4_dz_1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <h1>domashka_1_month_4</h1>
+            <Content/>
+            <Footer/>
+        </div>
+    );
 }
 
+
+function Header () {
+    return (
+        <div>
+            <h2> header component</h2>
+            <p>Lorem ipsum dolor sit</p>
+            <Title/>
+        </div>
+
+    )
+}
+
+function Footer() {
+    return (
+        <>
+            <h3>footer component</h3>
+            <p>Lorem ipsum dolor.</p>
+            <Title/>
+        </>
+
+    )
+}
+
+function Content() {
+    return (
+        <>
+            <h4>content component</h4>
+            <p>Lorem ipsum dolor.</p>
+            <Title/>
+        </>
+
+    )
+}
+function Title () {
+    return (
+        <>
+            <h5>i am title </h5>
+        </>
+
+    )
+}
 export default App;
